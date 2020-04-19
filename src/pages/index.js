47fd579f2b44aaec1ea4e08aefa490e4857a8f16
Jennifer Davis <iennae@gmail.com>
2020-04-19 00:28:35 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,9 @@ export default () =>
     <Layout>
       <section>
         <p>
+          Due to COVID19, most CoffeeOps meetings have moved to virtual meetups. 
+        </p>
+        <p>
           Coffeehouses are hubs of information sharing: people gather, share
           thoughts, learn about happenings, and discuss challenges. Having a
           place and time to congregate allows people from diverse backgrounds
@@ -16,18 +19,6 @@ export default () =>
         <p>
           Coffeeops aims to bring on the Age of Delightenment fueled by coffee
           (tea or cocoa!), collaboration and cooperation across companies.
-          Coffeeops should be considered part of work, a critical part of
-          preventing stagnation and isolation of the daily grind and help
-          build resilience. Conferences provide some amount of this but are
-          limited in duration and can be too costly for an individual in time
-          and money.
-        </p>
-        <p>
-          The bigger your company, the more likely you need regular infusions
-          of external input. Groups working solely within an environment can
-          fail to recognize other ways of doing things, ignoring trends in
-          the industry, duplicating efforts, and expending time and energy
-          in areas that are not business differentiators.
         </p>
         <p>
           If there isn&apos;t a local Coffeeops already happening in your
